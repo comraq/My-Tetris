4714c1127ae8cf95d8d1d8e5b042a2a6d3ae260b
comraq <yinadam@hotmail.com>
2015-12-16 22:16:32 -0800

    started experimenting with different javascript animation options

--- js/gameFactory.js
+++ js/gameFactory.js
@@ -3,14 +3,21 @@ function Frame() {
   this.COLS = 10;
   this.x = 0;
   this.y = 0;
+
+  this.canvas;
+  this.context;
+  this.height;
+  this.width;
+  this.blockHeight;
+  this.blockWidth;
+
+  this.stopAuto = 0;
 };
 
 /* Draws a square at coordinates x, y */
 Frame.prototype.drawSquare = function(x, y) {
-  //this.context.rect(x, y, this.blockWidth - 1, this.blockHeight - 1);
-  this.context.rect(x, y, this.blockWidth - 1, 5);
-  this.context.stroke();
-  this.context.fill();
+  this.context.strokeRect(x, y, this.blockWidth - 1, this.blockHeight - 1);
+  this.context.fillRect(x, y, this.blockWidth - 1, this.blockHeight - 1);
 };
 
 Frame.prototype.setColours = function() {
@@ -21,6 +28,11 @@ Frame.prototype.setColours = function() {
 Frame.prototype.init = function() {
   this.canvas = document.getElementsByClassName("tetris-frame")[0];
   this.context = this.canvas.getContext("2d");
+
+  //Manually scaling up the canvas element size by the CSS transformed sizes
+  this.canvas.height = this.canvas.offsetHeight;
+  this.canvas.width = this.canvas.offsetWidth;
+
   this.height = this.canvas.height;
   this.width = this.canvas.width;
   this.blockHeight = this.height / this.ROWS;
@@ -34,6 +46,39 @@ Frame.prototype.getSize = function() {
 
 Frame.prototype.draw = function() {
   this.drawSquare(this.x, this.y);
-  this.x += this.blockWidth;
+  ++this.x;
+  //this.x += this.blockWidth;
   //this.y += this.blockHeight;
 };
+
+/* Clears all drawn elements from the canvas */
+Frame.prototype.clear = function() {
+  this.context.clearRect(0, 0, this.width, this.height);
+};
+
+Frame.prototype.reset = function() {
+  this.x = 0;
+  this.y = 0;
+};
+
+Frame.prototype.autoDraw = function() {
+  var frame = this;
+  
+  this.clear();
+  if (Math.round(this.x) >= this.width) {
+    this.reset();
+  };
+  this.draw();
+  this.stopAuto = requestAnimFrame(function() {frame.autoDraw()});
+};
+
+window.requestAnimFrame = (function() {
+	return  window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(callback){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
--- js/main.js
+++ js/main.js
@@ -2,8 +2,24 @@ var main = function() {
   var frame = new Frame();
   frame.init();
   frame.setColours();
-  $("#test-button").click(function() {frame.getSize()});
-  $("#test-button-2").click(function() {frame.draw()});
+
+  $("#get-size").click(function() {
+    frame.getSize();
+  });
+  $("#draw").click(function() {
+    frame.draw();
+  });
+  $("#clear").click(function() {
+    frame.clear();
+    frame.reset();
+  });
+  $("#auto-draw").click(function() {
+    frame.autoDraw(frame);
+  });  
+  $("#stop-auto").click(function() {
+    cancelAnimationFrame(frame.stopAuto);
+  });  
+
 };
 
 $(document).ready(main);
